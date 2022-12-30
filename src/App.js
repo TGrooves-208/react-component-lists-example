3e4624d33e6015dfb5f37524bc98f439103aed47
GAguilar208 <gaguilar208.accessibility@gmail.com>
2022-12-30 14:08:15 -0700

    feat: react example of lists with keys

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, Fragment } from "react";
+import "./App.css";
 
-function App() {
+// Goal is to take an array of some data
+// Converting that data and converting it to jsx components
+// That will be added to the DOM
+export default function App() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {/* We can use a javascipt expression to add an array */}
+        {/* This map function returns an array of list items */}
+        {/* This will present an error in the console for a duplicate key */}
+        {/* {items.map(item => {
+          return(
+            <li key={item}>{item}</li>
+          );
+        })} */}
+
+        {/* We can use an index as a workaround for a duplicated key if needed
+        This is a temporary fix but can be useful, keeping keys unique is the way to go
+        Using an index should be used as a last resort */}
+        {/* {items.map((item, index) => {
+          return(
+            <li key={index}>{item}</li>
+          );
+        })} */}
+
+        {/* Fragment edge case example, resulting in an error */}
+        {/* {items.map((item, index) => {
+          return (
+            <>
+              <li key={index}>{item}</li>
+              <li>Fragment example we no longer have key props</li>
+            </>
+          );
+        })} */}
+
+        {/* We can address this by using an actual React Fragment
+        What ever we add to the list will be rendered as the new Fragment of list items */}
+        {items.map((item, index) => {
+          return (
+            <Fragment key={index}>
+              <li>{item}</li>
+              <li>Fragment example we no longer have key props</li>
+            </Fragment>
+          );
+        })}
+      </ul>
+
+      {/* This is controlled by the newItem state */}
+      <input
+        type="text"
+        value={newItem}
+        onChange={(event) => setNewItem(event.target.value)}
+      />
+
+      {/* The items are going to be set to the previous items */}
+      {/* It will spread the previous items and then add the newItem */}
+      {/* The newItem will then be reset to an empty string '' */}
+      <button
+        onClick={() => {
+          setItems([...items, newItem]);
+          setNewItem("");
+        }}
+      >
+        Add List Item
+      </button>
     </div>
   );
 }
-
-export default App;
